2ed2025115e55a639a22d0fbd246892410d3d6ae
Chester Wood <pa@rigpa.me>
2018-12-27 16:52:07 -0700

    Push predictions from weather underground to couchdb

--- smartapps/chetstone/smart-thermostat-setpoints.src/smart-thermostat-setpoints.groovy
+++ smartapps/chetstone/smart-thermostat-setpoints.src/smart-thermostat-setpoints.groovy
@@ -23,6 +23,14 @@ definition(
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png") {
     appSetting "apiKey"
+    appSetting "account0"
+    appSetting "password0"
+    appSetting "url0"
+    appSetting "account1"
+    appSetting "password1"
+    appSetting "url1"
+
+
 }
 
 
@@ -31,7 +39,10 @@ preferences {
         input "thermostat", "capability.thermostat", required: true, title: "Thermostat"
         input "cloudyset", "number", required: true, title: "Cloudy Day Setpoint"
         input "sunnyset", "number", required: true, title: "Sunny Day Setpoint" 
-        input "threshold", "number", required: true, title: "Threshold (% sunny)" 
+        //input "testSunny", "number", required: true, title: "Fake predicted sunniness" 
+        input "cloudy_threshold", "number", required: true, title: "Force cloudy setpoint (% sunny)" 
+        input "sunny_threshold", "number", required: true, title: "Force sunny setpoint (% sunny)" 
+
         input "runtime", "time", required: true, title: "Time to execute every day"
 	}
 }
@@ -48,19 +59,86 @@ def updated() {
 	initialize()
 }
 
-def tomorrowHowSunny(def startHour, def endHour) {
+def parseHttpResponse(response) {
+	def code = response.getStatus();
+    log.debug "HTTP Response: ${code}"
+//            response.headers.each {
+//            log.debug "${it.name} : ${it.value}"
+//        }
+	log.debug "HTTP Data: ${response.data}"
+    if (code > 399) {
+    	def dataStr = response.data;
+    	sendPush("TemperatureLogger got HTTP response ${code}")
+    }
+
+}
+
+def writeChannelData( value) {
+    def now = new Date()
+    def id = now.format("yyyy-MM-dd'T'HH:mm:ss'Z'") + ".Notes"
+    log.debug(id )
+	
+
+	def uri = "https://${appSettings.account0}:${appSettings.password0}@${appSettings.url0}";
+	def json = "{\"docs\":[{\"_id\":\"${id}\",\"predictions\":\"From groovy\",\"wxunderground\":${value}}]}";
+	def headers = [
+        "Content-Type" : "application/json"
+    ];
+	def params = [
+        uri: uri,
+        headers: headers,
+        body: json,
+        path: "_bulk_docs"
+    ];
+  
+    log.debug(params)
+    try {
+        httpPostJson(params) {response -> parseHttpResponse(response)}
+    }
+    catch(e) {
+        log.debug "httpPost threw ${e}"
+        }
+    // Also post to Digital Ocean
+    uri = "https://${appSettings.account1}:${appSettings.password1}@${appSettings.url1}"
+    
+    params = [
+        uri: uri,
+        headers: headers,
+        body: json,
+        path: "_bulk_docs"
+    ]
+    log.debug(params)
+    try {
+        httpPostJson(params) {response -> parseHttpResponse(response)}
+    }
+    catch(e) {
+          log.debug "httpPost threw ${e}"
+    }
+}
+
+// 
+
+
+
+def tomorrowHowSunny() {
+	def startHour = 8
+	def endHour = 16
     def sum = 0
     def count = 0
-    Map sdata = getWeatherFeature('hourly', 'pws:KCOMOFFA6');
+    def couch_count = 0
+    def couch_sum = 0
+    Map sdata = getWeatherFeature('hourly', 'pws:KCOCREST1');
  
     if(sdata == null || sdata.response == null || sdata.response.containsKey('error') ) {
     	sendNotificationEvent("Weather API error ${sdata?.response?.error}, setting setpoint assuming cloudy tomorrow")
         log.debug "Error ${sdata.response.error}"
         return 0
     }
-
+	log.debug "Hourly Forecast: ${sdata.hourly_forecast[0].FCTTIME}"
     def fcst = sdata.hourly_forecast
     def today = fcst[0].FCTTIME.weekday_name
+    // weight mornings higher
+    def times = [startHour, 9, 9, 10, 10, 10, 11, 11, 12, 13, 14, 15, endHour]
 
     for (int i=0; i < fcst.size(); i += 1) {
         if ( fcst[i].FCTTIME.weekday_name == today ) {
@@ -70,25 +148,40 @@ def tomorrowHowSunny(def startHour, def endHour) {
             if (fcst[i].FCTTIME.hour.toInteger() >= endHour) {
                 break
             }
-            count += 1
-            sum += 100 - fcst[i].sky.toInteger()
-            log.debug "hour: ${fcst[i].FCTTIME.hour} sunny: ${100 - fcst[i].sky.toInteger()}"
+            for (int j=0; j < times.size(); j += 1) {
+            	if (fcst[i].FCTTIME.hour.toInteger() == times[j]) {
+		            count += 1
+        		    sum += 100 - fcst[i].sky.toInteger()
+            //		log.debug "hour: ${fcst[i].FCTTIME.hour} sunny: ${100 - fcst[i].sky.toInteger()}"
+                }
+            }
+            // get unscaled sum for pushing to couchdb
+            couch_count += 1
+            couch_sum += 100 - fcst[i].sky.toInteger()
+           // log.debug "hour: ${fcst[i].FCTTIME.hour} sunny: ${100 - fcst[i].sky.toInteger()}"
         }
     }
     if (count == 0 || sum == 0) {
+    log.debug "Returning 0 and not sending to couch"
         return 0
     }
+    log.debug "Unscaled prediction: ${couch_sum/couch_count}"
+    writeChannelData(couch_sum/couch_count)
     sum/count
 }
 
 def setSetpoint(howSunny) {
-    def setpoint = howSunny > threshold ? sunnyset : cloudyset
+    def setpoint = howSunny >= sunny_threshold ? sunnyset :
+    howSunny <= cloudy_threshold ? cloudyset : 
+    	cloudyset + ((howSunny - cloudy_threshold)/(sunny_threshold - cloudy_threshold)*(sunnyset - cloudyset))
+    setpoint = setpoint.toInteger()
+    log.debug "Setpoint: ${setpoint}"
     settings.thermostat.setHeatingSetpoint(setpoint)
     setpoint
 }
 
 def handler() {
-    float sunniness = tomorrowHowSunny(9, 16)
+    float sunniness = tomorrowHowSunny()
     int setpoint = setSetpoint(sunniness)
     sendNotificationEvent(
         "Tomorrow will be sunny ${sunniness.round()}% of the time. Setting setpoint to ${setpoint}℉")
@@ -97,6 +190,7 @@ def handler() {
 
 def initialize() {
     schedule(runtime, handler)
-    float sunny = tomorrowHowSunny(9, 16)
+    float sunny = tomorrowHowSunny()
+    setSetpoint(sunny)
     log.debug "${sunny.round()}"
 }
